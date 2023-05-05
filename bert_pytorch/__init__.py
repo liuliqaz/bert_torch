a95aa0909d86d3ee859f394a54c406d637a0e0dc
liuli <2523008742@qq.com>
2023-05-05 12:07:39 +0000

    init

---
+++ bert_pytorch/__init__.py
@@ -0,0 +1,5 @@
+from .model import BERT
+from .model import BERTLM
+from .trainer.optim_schedule import ScheduledOptim
+from .dataset.vocab import WordVocab
+from .dataset.dataset import BERTDataset
